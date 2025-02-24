ab6b076fc444fa6bee46bc7e780786041cf1f389
totorotail <jminkyun@gmail.com>
2025-02-25 00:47:29 +0900

    feat: Navbar 영역 및 아이콘 추가

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
+  import Navbar from './components/Navbar.vue';
 
 </script>
 
 <template>
-
+  <Navbar/>
+  <font-awesome-icon :icon="['fas', 'location-dot']" />
 </template>
 
 <style scoped lang="scss">
