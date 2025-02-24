2acbb7b6e27d7c87162f272d06338965af0a3281
totorotail <jminkyun@gmail.com>
2025-02-25 02:03:29 +0900

    feat: 날씨정보 가져오기, 환경변수 추가

--- src/components/MainComp.vue
+++ src/components/MainComp.vue
@@ -1,11 +1,17 @@
 <template>
-    <div class="container">
-      <SearchBar/>
-      <WeatherInfo/>
-    </div>
-  </template>
-  
-  <script setup>
-    import SearchBar from './SearchBar.vue';
-    import WeatherInfo from './WeatherInfo.vue';
-  </script>
+  <div class="container">
+    <SearchBar/>
+    <WeatherInfo/>
+  </div>
+</template>
+
+<script setup>
+  import SearchBar from './SearchBar.vue';
+  import WeatherInfo from './WeatherInfo.vue';
+</script>
+
+<style lang="scss" scoped>
+  .container {
+    padding: 20px;
+  }
+</style>
