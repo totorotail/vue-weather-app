3f19b91d0857064dd62023c926f35657c77d1b63
totorotail <jminkyun@gmail.com>
2025-02-26 00:37:16 +0900

    chore: vuex에서 pinia로 마이그레이션

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,23 @@
   import MainComp from './components/MainComp.vue';
   import About from './components/About.vue';
   import { ref, onMounted } from 'vue';
-  import { useStore } from 'vuex';
+  // import { useStore } from 'vuex';
+  import { useStore } from './store/store';
+  import { storeToRefs } from 'pinia';
  
   const store = useStore();
+  const { toggle } = storeToRefs(store);
 
   // 앱이 실행되면 날씨 데이터 가져오기
   onMounted(() => {
-    store.dispatch('getWeather');
+    // store.dispatch('getWeather');
+    store.getWeather();
   })
 </script>
 
 <template>
   <Navbar/>
-  <div v-if="!$store.state.toggle">
+  <div v-if="!toggle">
     <MainComp/>
   </div>
   <div v-else>
--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,9 @@
-import { createStore } from 'vuex';
+import { defineStore } from 'pinia';
 
 // store 만들기
-export default createStore({
-  state: {
-    // initial state
-    count: 0,
+export const useStore = defineStore('main', {
+  state: () => ({
+    // 상태변수 정의
     weatherData: {
       icon: 'icon',
       temp: 0,
@@ -13,36 +12,36 @@ export default createStore({
       city: 'Seoul',
     },
     toggle: false, // true일때 about을 보여주기
-  },
-  mutations: {
-    // mutations(데이터 변경)
-    addCount(state, palyload) { 
-      state.count += 1 + palyload;
+  }),
+  actions: {
+    // 함수
+    addCount(palyload) { 
+      this.count += 1 + palyload;
     },
-    updateWeather(state, payload) {
-      state.weatherData.icon = payload.weather[0].icon;
-      state.weatherData.temp = payload.main.temp;
-      state.weatherData.text = payload.weather[0].description;
-      state.weatherData.location = payload.sys.country;
-      state.weatherData.city = payload.name;
+    updateWeather(payload) {
+      this.weatherData.icon = payload.weather[0].icon;
+      this.weatherData.temp = payload.main.temp;
+      this.weatherData.text = payload.weather[0].description;
+      this.weatherData.location = payload.sys.country;
+      this.weatherData.city = payload.name;
     },
-    onSearchCity(state, payload) {
-      state.weatherData.city = payload;
+    onSearchCity(payload) {
+      this.weatherData.city = payload;
     },
     toggleButton (state) {
-      state.toggle = !state.toggle;
-    }
-  },
-  actions: {
-    getWeather(context) {
+      this.toggle = !this.toggle;
+    },
+    // 비동기 함수 async 
+    async getWeather() {
       const API_KEY = import.meta.env.VITE_API_KEY;
-      const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${context.state.weatherData.city}&appid=${API_KEY}`
-      fetch(API_URL)
+      const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${this.weatherData.city}&appid=${API_KEY}`
+      await fetch(API_URL)
         .then(res => res.json())
         .then(data => {
           console.log(data);
           // mutation 함수로 날씨 정보 업데이트
-          context.commit('updateWeather', data);
+          // context.commit('updateWeather', data);
+          this.updateWeather(data);
         })
         .catch(err => {
           alert('에러가 발생했습니다. 잠시 후 다시 시도해 주세요.');
